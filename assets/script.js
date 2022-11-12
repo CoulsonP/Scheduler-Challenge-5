1c060dfdc2b7e6201f3659f8bf37e8b814938482
CoulsonP <coulsonp@icloud.com>
2022-11-12 13:53:58 -0600

    added local storage for all text areas

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,13 @@ $(".saveBtn").on("click",function(){
     var divKey = $(this).parent().attr("id")
     window.localStorage.setItem(divKey,textValue)
 })
+// on page load grab notes from local storage to display an appropiate time block
+$("#9 textarea").val(localStorage.getItem("9"))
+$("#10 textarea").val(localStorage.getItem("10"))
+$("#11 textarea").val(localStorage.getItem("11"))
+$("#12 textarea").val(localStorage.getItem("12"))
+$("#13 textarea").val(localStorage.getItem("13"))
+$("#14 textarea").val(localStorage.getItem("14"))
+$("#15 textarea").val(localStorage.getItem("15"))
+$("#16 textarea").val(localStorage.getItem("16"))
+$("#17 textarea").val(localStorage.getItem("17"))
